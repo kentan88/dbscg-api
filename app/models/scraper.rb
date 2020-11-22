04079438eb35eb73812fc04ab350bcae8a9cd0f0
Ken Tan <ken.tan@thoughtworks.com>
2020-11-23 03:00:24 +0800

    wip

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -1,133 +1,134 @@
 require 'nokogiri'
 require 'watir'
 require 'json'
+require "open-uri"
 
 SERIES_LIST = [
-    {
-        title: "DBS-B11-Booster-Vermilion-Bloodline",
-        series: "DBS-B11",
-        category: "428011"
-    },
-    {
-        title: "DBS-B10-Booster-Rise-Of-The-Unison-Warrior",
-        series: "DBS-B10",
-        category: "428010"
-    },
-    {
-        title: "DBS-B09-Universal-Onslaught",
-        series: "DBS-B09",
-        category: "428009"
-    },
-    {
-        title: "DBS-B08-Malicious-Machinations",
-        series: "DBS-B08",
-        category: "428008"
-    },
-    {
-        title: "DBS-B07-Assault-Of-The-Saiyans",
-        series: "DBS-B07",
-        category: "428007"
-    },
-    {
-        title: "DBS-B06-Destroyer-Kings",
-        series: "DBS-B06",
-        category: "428006"
-    },
-    {
-        title: "DBS-B05-Miraculous-Revival",
-        series: "DBS-B05",
-        category: "428005"
-    },
-    {
-        title: "DBS-B04-Colossal-Warfare",
-        series: "DBS-B04",
-        category: "428004"
-    },
-    {
-        title: "DBS-B03-Cross-Worlds",
-        series: "DBS-B03",
-        category: "428003"
-    },
-    {
-        title: "DBS-B02-Union-Force",
-        series: "DBS-B02",
-        category: "428002"
-    },
-    {
-        title: "DBS-B01-Galactic-Battle",
-        series: "DBS-B01",
-        category: "428001"
-    },
-    {
-        title: "DBS-TB03-Clash-Of-Fates",
-        series: "DBS-TB03",
-        category: "428103"
-    },
-    {
-        title: "DBS-TB02-World-Martial-Arts-Tournament",
-        series: "DBS-TB02",
-        category: "428102"
-    },
-    {
-        title: "DBS-TB01-The-Tournament-Of-Power",
-        series: "DBS-TB01",
-        category: "428101"
-    },
-    {
-        title: "DBS-Draft-Box-06-Giant-Force",
-        series: "Draft-Box-06",
-        category: "428603"
-    },
-    {
-        title: "DBS-Draft-Box-05-Divine-Multiverse",
-        series: "Draft-Box-05",
-        category: "428602"
-    },
-    {
-        title: "DBS-Draft-Box-04-Dragon-Brawl",
-        series: "Draft-Box-04",
-        category: "428601"
-    },
-    {
-        title: "DBS-SD14-Saiyan Wonder",
-        series: "DBS-SD14",
-        category: "428314"
-    },
-    {
-        title: "DBS-SD13-Clan Collusion",
-        series: "DBS-SD13",
-        category: "428313"
-    },
-    {
-        title: "DBS-SD12-Spirit-Of-Potara",
-        series: "DBS-SD12",
-        category: "428312"
-    },
-    {
-        title: "DBS-SD11-Instinct-Surpassed",
-        series: "DBS-SD11",
-        category: "428311"
-    },
-    {
-        title: "DBS-SD10-Parasitic-Overlord",
-        series: "DBS-SD10",
-        category: "428310"
-    },
-    {
-        title: "DBS-SD09-Saiyan-Legacy",
-        series: "DBS-SD09",
-        category: "428309"
-    },
-    {
-        title: "DBS-SD08-Rising-Broly",
-        series: "DBS-SD08",
-        category: "428308"
-    },
-    {
-        title: "DBS-SD07-Shenron's-Advent",
-        series: "DBS-SD07",
-        category: "428307"
-    },
+    # {
+    #     title: "DBS-B11-Booster-Vermilion-Bloodline",
+    #     series: "DBS-B11",
+    #     category: "428011"
+    # },
+    # {
+    #     title: "DBS-B10-Booster-Rise-Of-The-Unison-Warrior",
+    #     series: "DBS-B10",
+    #     category: "428010"
+    # },
+    # {
+    #     title: "DBS-B09-Universal-Onslaught",
+    #     series: "DBS-B09",
+    #     category: "428009"
+    # },
+    # {
+    #     title: "DBS-B08-Malicious-Machinations",
+    #     series: "DBS-B08",
+    #     category: "428008"
+    # },
+    # {
+    #     title: "DBS-B07-Assault-Of-The-Saiyans",
+    #     series: "DBS-B07",
+    #     category: "428007"
+    # },
+    # {
+    #     title: "DBS-B06-Destroyer-Kings",
+    #     series: "DBS-B06",
+    #     category: "428006"
+    # },
+    # {
+    #     title: "DBS-B05-Miraculous-Revival",
+    #     series: "DBS-B05",
+    #     category: "428005"
+    # },
+    # {
+    #     title: "DBS-B04-Colossal-Warfare",
+    #     series: "DBS-B04",
+    #     category: "428004"
+    # },
+    # {
+    #     title: "DBS-B03-Cross-Worlds",
+    #     series: "DBS-B03",
+    #     category: "428003"
+    # },
+    # {
+    #     title: "DBS-B02-Union-Force",
+    #     series: "DBS-B02",
+    #     category: "428002"
+    # },
+    # {
+    #     title: "DBS-B01-Galactic-Battle",
+    #     series: "DBS-B01",
+    #     category: "428001"
+    # },
+    # {
+    #     title: "DBS-TB03-Clash-Of-Fates",
+    #     series: "DBS-TB03",
+    #     category: "428103"
+    # },
+    # {
+    #     title: "DBS-TB02-World-Martial-Arts-Tournament",
+    #     series: "DBS-TB02",
+    #     category: "428102"
+    # },
+    # {
+    #     title: "DBS-TB01-The-Tournament-Of-Power",
+    #     series: "DBS-TB01",
+    #     category: "428101"
+    # },
+    # {
+    #     title: "DBS-Draft-Box-06-Giant-Force",
+    #     series: "Draft-Box-06",
+    #     category: "428603"
+    # },
+    # {
+    #     title: "DBS-Draft-Box-05-Divine-Multiverse",
+    #     series: "Draft-Box-05",
+    #     category: "428602"
+    # },
+    # {
+    #     title: "DBS-Draft-Box-04-Dragon-Brawl",
+    #     series: "Draft-Box-04",
+    #     category: "428601"
+    # },
+    # {
+    #     title: "DBS-SD14-Saiyan Wonder",
+    #     series: "DBS-SD14",
+    #     category: "428314"
+    # },
+    # {
+    #     title: "DBS-SD13-Clan Collusion",
+    #     series: "DBS-SD13",
+    #     category: "428313"
+    # },
+    # {
+    #     title: "DBS-SD12-Spirit-Of-Potara",
+    #     series: "DBS-SD12",
+    #     category: "428312"
+    # },
+    # {
+    #     title: "DBS-SD11-Instinct-Surpassed",
+    #     series: "DBS-SD11",
+    #     category: "428311"
+    # },
+    # {
+    #     title: "DBS-SD10-Parasitic-Overlord",
+    #     series: "DBS-SD10",
+    #     category: "428310"
+    # },
+    # {
+    #     title: "DBS-SD09-Saiyan-Legacy",
+    #     series: "DBS-SD09",
+    #     category: "428309"
+    # },
+    # {
+    #     title: "DBS-SD08-Rising-Broly",
+    #     series: "DBS-SD08",
+    #     category: "428308"
+    # },
+    # {
+    #     title: "DBS-SD07-Shenron's-Advent",
+    #     series: "DBS-SD07",
+    #     category: "428307"
+    # },
     {
         title: "DBS-SD06-Resurrected-Fusion",
         series: "DBS-SD06",
@@ -265,6 +266,11 @@ class Scraper
           skills_nodes = card_front_nodes.css('.skillCol')
 
           number = number_nodes.inner_text
+
+          File.open(Rails.root.join('public', 'images', "#{number}.png"), 'wb') do |fo|
+            fo.write open("http://www.dbs-cardgame.com/images/cardlist/cardimg/#{number}.png").read
+          end
+
           title = title_nodes.inner_text
           series_list = series_nodes.children[3].inner_html.split("<br>")
           rarity = rarity_nodes.children[3].inner_text
@@ -301,7 +307,7 @@ class Scraper
           card['era'] = era_nodes.children[3].inner_text if era_nodes.children.length > 0
 
           if skills_nodes.children.length > 0
-            card['skills_text'] = skills_nodes.children[3].inner_html.gsub("../..", "http://www.dbs-cardgame.com").gsub("<br>", "<br><br>")
+            card['skills_text'] = skills_nodes.children[3].inner_html.gsub("../..", "http://www.dbs-cardgame.com")
             card['skills'] = skills_nodes.children[3].inner_html.scan(/[a-zA-Z-]+.png/).map { |str| str.gsub(".png", "") }.uniq
           end
 
@@ -316,7 +322,7 @@ class Scraper
             card['title_back'] = title_back_nodes.inner_text
 
             if skills_back_nodes.children.length > 0
-              card['skills_back_text'] = skills_back_nodes.children[3].inner_html.gsub("../..", "http://www.dbs-cardgame.com").gsub("<br>", "<br><br>")
+              card['skills_back_text'] = skills_back_nodes.children[3].inner_html.gsub("../..", "http://www.dbs-cardgame.com")
               card['skills_back'] = skills_back_nodes.children[3].inner_html.scan(/[a-zA-Z\-_]+.png/).map { |str| str.gsub(".png", "") }.uniq
             end
           end
