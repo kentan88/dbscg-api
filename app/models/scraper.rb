2594c22b49a111580b4050e204283ffb2aacb5a9
Ken Tan <ken.tan@thoughtworks.com>
2020-11-23 10:17:51 +0800

    wip

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -4,131 +4,131 @@ require 'json'
 require "open-uri"
 
 SERIES_LIST = [
-    # {
-    #     title: "DBS-B11-Booster-Vermilion-Bloodline",
-    #     series: "DBS-B11",
-    #     category: "428011"
-    # },
-    # {
-    #     title: "DBS-B10-Booster-Rise-Of-The-Unison-Warrior",
-    #     series: "DBS-B10",
-    #     category: "428010"
-    # },
-    # {
-    #     title: "DBS-B09-Universal-Onslaught",
-    #     series: "DBS-B09",
-    #     category: "428009"
-    # },
-    # {
-    #     title: "DBS-B08-Malicious-Machinations",
-    #     series: "DBS-B08",
-    #     category: "428008"
-    # },
-    # {
-    #     title: "DBS-B07-Assault-Of-The-Saiyans",
-    #     series: "DBS-B07",
-    #     category: "428007"
-    # },
-    # {
-    #     title: "DBS-B06-Destroyer-Kings",
-    #     series: "DBS-B06",
-    #     category: "428006"
-    # },
-    # {
-    #     title: "DBS-B05-Miraculous-Revival",
-    #     series: "DBS-B05",
-    #     category: "428005"
-    # },
-    # {
-    #     title: "DBS-B04-Colossal-Warfare",
-    #     series: "DBS-B04",
-    #     category: "428004"
-    # },
-    # {
-    #     title: "DBS-B03-Cross-Worlds",
-    #     series: "DBS-B03",
-    #     category: "428003"
-    # },
-    # {
-    #     title: "DBS-B02-Union-Force",
-    #     series: "DBS-B02",
-    #     category: "428002"
-    # },
-    # {
-    #     title: "DBS-B01-Galactic-Battle",
-    #     series: "DBS-B01",
-    #     category: "428001"
-    # },
-    # {
-    #     title: "DBS-TB03-Clash-Of-Fates",
-    #     series: "DBS-TB03",
-    #     category: "428103"
-    # },
-    # {
-    #     title: "DBS-TB02-World-Martial-Arts-Tournament",
-    #     series: "DBS-TB02",
-    #     category: "428102"
-    # },
-    # {
-    #     title: "DBS-TB01-The-Tournament-Of-Power",
-    #     series: "DBS-TB01",
-    #     category: "428101"
-    # },
-    # {
-    #     title: "DBS-Draft-Box-06-Giant-Force",
-    #     series: "Draft-Box-06",
-    #     category: "428603"
-    # },
-    # {
-    #     title: "DBS-Draft-Box-05-Divine-Multiverse",
-    #     series: "Draft-Box-05",
-    #     category: "428602"
-    # },
-    # {
-    #     title: "DBS-Draft-Box-04-Dragon-Brawl",
-    #     series: "Draft-Box-04",
-    #     category: "428601"
-    # },
-    # {
-    #     title: "DBS-SD14-Saiyan Wonder",
-    #     series: "DBS-SD14",
-    #     category: "428314"
-    # },
-    # {
-    #     title: "DBS-SD13-Clan Collusion",
-    #     series: "DBS-SD13",
-    #     category: "428313"
-    # },
-    # {
-    #     title: "DBS-SD12-Spirit-Of-Potara",
-    #     series: "DBS-SD12",
-    #     category: "428312"
-    # },
-    # {
-    #     title: "DBS-SD11-Instinct-Surpassed",
-    #     series: "DBS-SD11",
-    #     category: "428311"
-    # },
-    # {
-    #     title: "DBS-SD10-Parasitic-Overlord",
-    #     series: "DBS-SD10",
-    #     category: "428310"
-    # },
-    # {
-    #     title: "DBS-SD09-Saiyan-Legacy",
-    #     series: "DBS-SD09",
-    #     category: "428309"
-    # },
-    # {
-    #     title: "DBS-SD08-Rising-Broly",
-    #     series: "DBS-SD08",
-    #     category: "428308"
-    # },
-    # {
-    #     title: "DBS-SD07-Shenron's-Advent",
-    #     series: "DBS-SD07",
-    #     category: "428307"
-    # },
+    {
+        title: "DBS-B11-Booster-Vermilion-Bloodline",
+        series: "DBS-B11",
+        category: "428011"
+    },
+    {
+        title: "DBS-B10-Booster-Rise-Of-The-Unison-Warrior",
+        series: "DBS-B10",
+        category: "428010"
+    },
+    {
+        title: "DBS-B09-Universal-Onslaught",
+        series: "DBS-B09",
+        category: "428009"
+    },
+    {
+        title: "DBS-B08-Malicious-Machinations",
+        series: "DBS-B08",
+        category: "428008"
+    },
+    {
+        title: "DBS-B07-Assault-Of-The-Saiyans",
+        series: "DBS-B07",
+        category: "428007"
+    },
+    {
+        title: "DBS-B06-Destroyer-Kings",
+        series: "DBS-B06",
+        category: "428006"
+    },
+    {
+        title: "DBS-B05-Miraculous-Revival",
+        series: "DBS-B05",
+        category: "428005"
+    },
+    {
+        title: "DBS-B04-Colossal-Warfare",
+        series: "DBS-B04",
+        category: "428004"
+    },
+    {
+        title: "DBS-B03-Cross-Worlds",
+        series: "DBS-B03",
+        category: "428003"
+    },
+    {
+        title: "DBS-B02-Union-Force",
+        series: "DBS-B02",
+        category: "428002"
+    },
+    {
+        title: "DBS-B01-Galactic-Battle",
+        series: "DBS-B01",
+        category: "428001"
+    },
+    {
+        title: "DBS-TB03-Clash-Of-Fates",
+        series: "DBS-TB03",
+        category: "428103"
+    },
+    {
+        title: "DBS-TB02-World-Martial-Arts-Tournament",
+        series: "DBS-TB02",
+        category: "428102"
+    },
+    {
+        title: "DBS-TB01-The-Tournament-Of-Power",
+        series: "DBS-TB01",
+        category: "428101"
+    },
+    {
+        title: "DBS-Draft-Box-06-Giant-Force",
+        series: "Draft-Box-06",
+        category: "428603"
+    },
+    {
+        title: "DBS-Draft-Box-05-Divine-Multiverse",
+        series: "Draft-Box-05",
+        category: "428602"
+    },
+    {
+        title: "DBS-Draft-Box-04-Dragon-Brawl",
+        series: "Draft-Box-04",
+        category: "428601"
+    },
+    {
+        title: "DBS-SD14-Saiyan Wonder",
+        series: "DBS-SD14",
+        category: "428314"
+    },
+    {
+        title: "DBS-SD13-Clan Collusion",
+        series: "DBS-SD13",
+        category: "428313"
+    },
+    {
+        title: "DBS-SD12-Spirit-Of-Potara",
+        series: "DBS-SD12",
+        category: "428312"
+    },
+    {
+        title: "DBS-SD11-Instinct-Surpassed",
+        series: "DBS-SD11",
+        category: "428311"
+    },
+    {
+        title: "DBS-SD10-Parasitic-Overlord",
+        series: "DBS-SD10",
+        category: "428310"
+    },
+    {
+        title: "DBS-SD09-Saiyan-Legacy",
+        series: "DBS-SD09",
+        category: "428309"
+    },
+    {
+        title: "DBS-SD08-Rising-Broly",
+        series: "DBS-SD08",
+        category: "428308"
+    },
+    {
+        title: "DBS-SD07-Shenron's-Advent",
+        series: "DBS-SD07",
+        category: "428307"
+    },
     {
         title: "DBS-SD06-Resurrected-Fusion",
         series: "DBS-SD06",
@@ -229,6 +229,31 @@ SERIES_LIST = [
         series: "DBS-Expansion-Set-05",
         category: "428405"
     },
+    {
+        title: "DBS-Expansion-04-Unity-of-Saiyans",
+        series: "DBS-Expansion-Set-04",
+        category: "428404"
+    },
+    {
+        title: "DBS-Ultimate-Box",
+        series: "DBS-Ultimate-Box",
+        category: "428403"
+    },
+    {
+        title: "DBS-Expansion-02-Dark-Demons-Villains",
+        series: "DBS-Expansion-Set-02",
+        category: "428402"
+    },
+    {
+        title: "DBS-Expansion-01-Mighty-Heroes",
+        series: "DBS-Expansion-Set-01",
+        category: "428401"
+    },
+    {
+        title: "DBS-Promotion-Cards",
+        series: "DBS-Promotion-Cards",
+        category: "428901"
+    },
 ]
 
 class Scraper
@@ -267,9 +292,9 @@ class Scraper
 
           number = number_nodes.inner_text
 
-          File.open(Rails.root.join('public', 'images', "#{number}.png"), 'wb') do |fo|
-            fo.write open("http://www.dbs-cardgame.com/images/cardlist/cardimg/#{number}.png").read
-          end
+          # File.open(Rails.root.join('public', 'images', "#{number}.png"), 'wb') do |fo|
+          #   fo.write open("http://www.dbs-cardgame.com/images/cardlist/cardimg/#{number}.png").read
+          # end
 
           title = title_nodes.inner_text
           series_list = series_nodes.children[3].inner_html.split("<br>")
@@ -307,7 +332,7 @@ class Scraper
           card['era'] = era_nodes.children[3].inner_text if era_nodes.children.length > 0
 
           if skills_nodes.children.length > 0
-            card['skills_text'] = skills_nodes.children[3].inner_html.gsub("../..", "http://www.dbs-cardgame.com")
+            card['skills_text'] = skills_nodes.children[3].inner_html.gsub("../..", "../../images/cardlist/common")
             card['skills'] = skills_nodes.children[3].inner_html.scan(/[a-zA-Z-]+.png/).map { |str| str.gsub(".png", "") }.uniq
           end
 
@@ -322,7 +347,7 @@ class Scraper
             card['title_back'] = title_back_nodes.inner_text
 
             if skills_back_nodes.children.length > 0
-              card['skills_back_text'] = skills_back_nodes.children[3].inner_html.gsub("../..", "http://www.dbs-cardgame.com")
+              card['skills_back_text'] = skills_back_nodes.children[3].inner_html.gsub("../../images/cardlist/common", "/images")
               card['skills_back'] = skills_back_nodes.children[3].inner_html.scan(/[a-zA-Z\-_]+.png/).map { |str| str.gsub(".png", "") }.uniq
             end
           end
--- lib/tasks/images.rake
+++ lib/tasks/images.rake
@@ -1,34 +1,42 @@
-namespace :decks do
+namespace :images do
   task :seed => :environment do
-    [
-        "red_ball", "blue_ball", "black_ball", "yellow_ball", "green_ball",
-        "EX-Evolve", "Deflect", "Counter-Play", "Triple-Strike", "Auto",
-        "Permanent", "Counter", "Awaken", "Activate-Main", "Activate-Battle",
-        "Barrier", "Critical", "Super-Combo", "Counter-Play", "Blocker",
-        "Swap_2", "Swap_3", "Swap_4", "Swap_5", "Dark-Over-Realm", "Over-Realm",
-        "Over-Realm_4", "Over-Realm_3", "Over-Realm_2", "Over-Realm_5", "Dual-Attack",
-        "Energy-Exhaust", "Once-per-turn", "Arrival_Red-Green", "Awaken_Surge",
-        "Counter-Counter", "Counter-Attack", "Arrival_Red-Yellow", "Offering",
-        "Victory-Strike",
-        "UNISON_plus-minus_00",
-        "UNISON_plus_01",
-        "UNISON_plus_02",
-        "UNISON_minus_01",
-        "UNISON_minus_02",
-        "UNISON_minus_03",
-        "UNISON_minus_04",
-        "UNISON_minus_05",
-        "UNISON_minus_06",
-        "UNISON_minus_07",
-    ].each do |str|
-      File.open(Rails.root.join('public', 'images', "#{str}.png"), 'wb') do |fo|
-        fo.write open("http://www.dbs-cardgame.com/images/cardlist/common/#{str}.png").read
-      end
-    end
+    # [
+    #     "red_ball", "blue_ball", "black_ball", "yellow_ball", "green_ball",
+    #     "EX-Evolve", "Deflect", "Counter-Play", "Triple-Strike", "Auto",
+    #     "Permanent", "Counter", "Awaken", "Activate-Main", "Activate-Battle",
+    #     "Barrier", "Critical", "Super-Combo", "Counter-Play", "Blocker",
+    #     "Swap_2", "Swap_3", "Swap_4", "Swap_5", "Dark-Over-Realm", "Over-Realm",
+    #     "Over-Realm_4", "Over-Realm_3", "Over-Realm_2", "Over-Realm_5", "Dual-Attack",
+    #     "Energy-Exhaust", "Once-per-turn", "Arrival_Red-Green", "Awaken_Surge",
+    #     "Counter-Counter", "Counter-Attack", "Arrival_Red-Yellow", "Offering",
+    #     "Victory-Strike",
+    #     "UNISON_plus-minus_00",
+    #     "UNISON_plus_01",
+    #     "UNISON_plus_02",
+    #     "UNISON_minus_01",
+    #     "UNISON_minus_02",
+    #     "UNISON_minus_03",
+    #     "UNISON_minus_04",
+    #     "UNISON_minus_05",
+    #     "UNISON_minus_06",
+    #     "UNISON_minus_07",
+    # ].each do |str|
+    #   File.open(Rails.root.join('public', 'images', "#{str}.png"), 'wb') do |fo|
+    #     fo.write open("http://www.dbs-cardgame.com/images/cardlist/common/#{str}.png").read
+    #   end
+    # end
 
     Card.all.each do |card|
-      File.open(Rails.root.join('public', 'images', "#{card.number}.png"), 'wb') do |fo|
-        fo.write open("http://www.dbs-cardgame.com/images/cardlist/cardimg/#{card.number}.png").read
+      path = Rails.root.join('public', 'images', "#{card.number}.png")
+      puts "card id: #{card.id}"
+
+      if File.file?(path)
+        puts "image exist skip"
+      else
+        File.open(path, 'wb') do |fo|
+          puts "downloading"
+          fo.write open("http://www.dbs-cardgame.com/images/cardlist/cardimg/#{card.number}.png").read
+      end
       end
     end
   end
