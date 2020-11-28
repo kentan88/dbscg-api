08fe148e6504a1f34b47e36104f64da55369cc7f
Ken Tan <ken.tan@thoughtworks.com>
2020-11-28 21:22:15 +0800

    added description to decks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_094754) do
+ActiveRecord::Schema.define(version: 2020_11_28_132038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_11_28_094754) do
     t.bigint "card_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["card_id"], name: "index_decks_on_card_id"
     t.index ["user_id"], name: "index_decks_on_user_id"
   end
