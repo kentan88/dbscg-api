d9067a522e149a1e45fb538e7348a3234e60e743
Ken Tan <ken.tan@thoughtworks.com>
2020-11-28 18:24:46 +0800

    update scraper

--- app/controllers/api/leaders_controller.rb
+++ app/controllers/api/leaders_controller.rb
@@ -1,4 +1,4 @@
-class API::CardsController < ApplicationController
+class API::LeadersController < ApplicationController
   def index
     @q = Leader.order(title: :asc).ransack(params[:q])
     @leaders = @q.result(distinct: true).page(params[:page]).per(25)
