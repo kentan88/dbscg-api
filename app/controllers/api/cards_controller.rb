34af8829728a439c82c0f4232cec5fcced381efd
Ken Tan <ken.tan@thoughtworks.com>
2020-11-29 20:55:16 +0800

    wi

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,9 +1,6 @@
 class API::CardsController < ApplicationController
   def index
-    # @q = Card.order(energy_cost: :asc, title: :asc).ransack(params[:q])
-    # @cards = @q.result(distinct: true).page(params[:page]).per(25)
-    #
-    #
-    @cards = Card.all.page(1).per(10000)
+    @q = Card.order(energy_cost: :asc, title: :asc).ransack(params[:q])
+    @cards = @q.result(distinct: true).page(params[:page]).per(25)
   end
 end
