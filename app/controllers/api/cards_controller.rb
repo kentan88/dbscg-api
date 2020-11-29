23fa27c087ac80121f076bdf478b3772123c521f
Ken Tan <ken.tan@thoughtworks.com>
2020-11-29 09:15:15 +0800

    prettier

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,6 +1,6 @@
 class API::CardsController < ApplicationController
   def index
-    @q = Card.order(title: :asc).ransack(params[:q])
+    @q = Card.order(energy_cost: :asc).ransack(params[:q])
     @cards = @q.result(distinct: true).page(params[:page]).per(25)
   end
 end
