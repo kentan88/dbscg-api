6599b0a674a1a44033001c3b35c8b58d2adcce6b
Ken Tan <ken.tan@thoughtworks.com>
2020-12-03 11:17:11 +0800

    wip

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,10 +2,10 @@ class API::UsersController < ApplicationController
   before_action :extract_user_id_from_token, only: [:info]
 
   def info
-    @user = User.find(@user_id)
+    @user = User.find(@user_id) if @user_id
     cards = Card.where.not(rating: 0)
     ratings = Hash[cards.collect { |item| [item.number, item.rating] } ]
-    album = @user.album.data
+    album = @user.try(:album).try(:data)
 
     render json: { ratings: ratings, album: album }
   end
