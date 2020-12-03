48c872d36bb4d1c78609941f71d83279dc3a1c0b
Ken Tan <ken.tan@thoughtworks.com>
2020-12-04 02:11:37 +0800

    fix migrations

--- db/migrate/20201201014023_create_albums.rb
+++ db/migrate/20201201014023_create_albums.rb
@@ -2,7 +2,7 @@ class CreateAlbums < ActiveRecord::Migration[6.0]
   def change
     create_table :albums do |t|
       t.references :user, null: false, foreign_key: true
-      t.jsonb :data, default: '{}'
+      t.jsonb :data
     end
   end
 end
