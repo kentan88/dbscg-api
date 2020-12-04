188a65981dcf59028424d789647b614f35cbcc50
Ken Tan <ken.tan@thoughtworks.com>
2020-12-05 03:38:09 +0800

    wip

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class API::UsersController < ApplicationController
   before_action :extract_user_id_from_token, only: [:info]
 
   def info
-    @user = User.find(@user_id) if @user_id
+    @user = User.find(@user_id) if @user_id rescue nil
     cards = Card.where.not(rating: 0)
     ratings = Hash[cards.collect { |item| [item.number, item.rating] } ]
     album = @user.try(:album).try(:data)
