24b7ec16da7e1e070d5b3739a5c49adffb5a725c
Ken Tan <ken.tan@thoughtworks.com>
2020-12-09 01:40:31 +0800

    fix

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -3,6 +3,12 @@ class API::AlbumsController < ApplicationController
 
   def update
     @album = Album.find_by(user_id: @user_id)
+
+    unless @album.present?
+      render :status => 400, :json => {:message => 'Unauthorized'}
+      return
+    end
+
     data = @album.data.as_json
 
     card_number = params[:card_number].to_s
@@ -10,5 +16,7 @@ class API::AlbumsController < ApplicationController
 
     @album.data = data
     @album.update_column(:data, @album.data)
+
+    render json: {success: true}
   end
 end
