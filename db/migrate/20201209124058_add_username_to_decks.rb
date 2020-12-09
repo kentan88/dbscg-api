6a7386a7c7df64a9a914d123e26b53afc1466c8c
Ken Tan <ken.tan@thoughtworks.com>
2020-12-10 02:40:55 +0800

    refactor big bang

--- db/migrate/20201209124058_add_username_to_decks.rb
+++ db/migrate/20201209124058_add_username_to_decks.rb
@@ -1,18 +1,5 @@
 class AddUsernameToDecks < ActiveRecord::Migration[6.0]
   def change
     add_column :decks, :username, :string
-
-    decks = Deck.includes(:user, :deck_cards)
-
-    decks.each do |deck|
-      main_deck_cards = Hash[deck.deck_cards.where(type: "main").collect { |dc| [dc.number, dc.quantity] }]
-      side_deck_cards = Hash[deck.deck_cards.where(type: "side").collect { |dc| [dc.number, dc.quantity] }]
-
-      deck.update_columns({
-                              main_deck_cards: main_deck_cards,
-                              side_deck_cards: side_deck_cards,
-                              username: deck.user.username
-                          })
-    end
   end
 end
