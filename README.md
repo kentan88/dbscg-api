6830c3462e285817ea3d56bb214fa63d98fbd6eb
Ken Tan <ken.tan@thoughtworks.com>
2020-12-11 17:50:59 +0800

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d dbsuper_development latest.dump
+
 http://localhost:3001/api/cards.json?q[title_cont]=Goku
 http://localhost:3001/api/cards.json?q[color_matches_any][]=Red&q[color_matches_any][]=Blue
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# require 'json'
-
-# DeckCard.delete_all
-# Deck.delete_all
-# Card.delete_all
-#
-User.create(username: "kentan", email: "kentan0130@gmail.com", password: 11111111, password_confirmation: 11111111)
-
 Dir.glob("#{Rails.root}/public/data/**").sort.each do |filename|
   file = File.read(filename)
   data_hash = JSON.parse(file)
