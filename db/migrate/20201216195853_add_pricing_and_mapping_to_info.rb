8778f4926ca7f6774335b2e728683b61562019ca
Ken Tan <ken.tan@thoughtworks.com>
2020-12-17 04:04:34 +0800

    wip

--- db/migrate/20201216195853_add_pricing_and_mapping_to_info.rb
+++ db/migrate/20201216195853_add_pricing_and_mapping_to_info.rb
@@ -1,8 +1,5 @@
 class AddPricingAndMappingToInfo < ActiveRecord::Migration[6.0]
   def change
-    remove_column :infos, :pricing
-    remove_column :infos, :tcg_mapping
-
     add_column :infos, :pricing, :json, array: true, default: []
     add_column :infos, :tcg_mapping, :jsonb, default: {}
   end
