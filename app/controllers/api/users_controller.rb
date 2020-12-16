791dadbe90b12d2453c74657ca3db82a2593473a
Ken Tan <ken.tan@thoughtworks.com>
2020-12-16 17:08:15 +0800

    wip

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@ class API::UsersController < ApplicationController
 
   def info
     @user = User.find(@user_id) if @user_id rescue nil
-    album = @user.album.data || {}
+    album = @user.album.data rescue {}
     ratings = Info.first.ratings
 
     render json: { album: album, ratings: ratings }
