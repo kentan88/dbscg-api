fce2db5169ba1e69ecd90305be69fdaa5b1360af
Ken Tan <ken.tan@thoughtworks.com>
2020-12-19 01:34:12 +0800

    update pricing api

--- app/controllers/api/pricings_controller.rb
+++ app/controllers/api/pricings_controller.rb
@@ -1,7 +1,13 @@
 class API::PricingsController < ApplicationController
   def index
-    info = Info.first
-    products = info.pricing
+    products = Info.first.pricing
+
+    deck_id = params[:deck_id]
+    if deck_id.present?
+      deck = Deck.find(deck_id)
+      card_numbers = deck.main_deck_cards.collect { |k, v| k }.concat(deck.side_deck_cards.collect { |k, v| k }).uniq
+      products = products.filter { |pricing| card_numbers.include?(pricing["number"]) }
+    end
 
     render json: { products: products }
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,36 +20,3 @@ end
 #   card.special_trait = card.special_trait.gsub("Universe7", "Universe 7")
 #   card.save
 # end
-
-["BT12-056",
-"BT12-057",
-"BT12-058",
-"BT12-059",
-"BT12-060",
-"BT12-061",
-"BT12-062",
-"BT12-063",
-"BT12-064",
-"BT12-065",
-"BT12-066",
-"BT12-067",
-"BT12-068",
-"BT12-069",
-"BT12-070",
-"BT12-071",
-"BT12-072",
-"BT12-073",
-"BT12-074",
-"BT12-075",
-"BT12-076",
-"BT12-077",
-"BT12-078",
-"BT12-079",
-"BT12-080",
-"BT12-081",
-"BT12-082",
-"BT12-083",
-"BT12-084"].each do |number|
-  card = Card.find_by(number: number)
-  card.update_column(:limit, 4)
-end
