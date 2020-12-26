9a924ca0c7574df8a789049b8631dd18e0d65ed3
Ken Tan <ken.tan@thoughtworks.com>
2020-12-26 14:32:20 +0800

    added filter by cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,6 @@ Dir.glob("#{Rails.root}/public/data/**").sort.each do |filename|
 
   puts cards_added.join(" ")
 end
+
+
+# [["King Piccolo, First Step to Revival", "Promotion Cards"], ["Surprise Attack Angila", "Promotion Cards"], ["Piccolo Jr., the King's Scion", "Promotion Cards"], ["Launch, Feminine Wiles", "Promotion Cards"], ["Launch, Feminine Wiles", "Promotion Cards"], ["Tapion, Savior From Another Time", "Promotion Cards"], ["Son Goku & Vegeta, Saiyan Synergy", "Promotion Cards"], ["Son Goku & Vegeta, Saiyan Synergy", "Promotion Cards"], ["Veku, the Unpredictable", "Promotion Cards"], ["Anti-Dimensional Slice", "Promotion Cards"], ["Lord Slug, Returned to Life", "Promotion Cards"], ["Frieza, Dark Power Unleashed", "Promotion Cards"], ["Vegeta, the Insurmountable", "Promotion Cards"], ["Master's Aid Whis", "Promotion Cards"], ["Omega Shenron, the Ultimate Shadow Dragon", "Promotion Cards"], ["Omega Shenron, the Ultimate Shadow Dragon", "Promotion Cards"], ["Nuova Shenron, Fair and Square", "Promotion Cards"], ["SS3 Gogeta, Martial Melee", "Promotion Cards"], ["SS3 Gogeta, Martial Melee", "Promotion Cards"], ["Gohanks, Apocalyptic Future", "Promotion Cards"], ["Supreme Kai of Time, Summoned from Another Dimension", "Promotion Cards"], ["Dark Masked King, Deluge of Darkness", "Promotion Cards"]]
