5aa98972768f50422aac0d4df2706e858535dbe0
Ken Tan <ken.tan@thoughtworks.com>
2021-02-28 16:17:48 +0800

    new cards

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,8 @@
-require "redis"
-
-if Rails.env.production?
-  puts "connecting to redis"
-  REDIS = Redis.new(url: ENV['REDIS_URL'])
-else
-  REDIS = Redis.new
-end
+# require "redis"
+#
+# if Rails.env.production?
+#   puts "connecting to redis"
+#   REDIS = Redis.new(url: ENV['REDIS_URL'])
+# else
+#   REDIS = Redis.new
+# end
